af14fcc01101c09a56800e3bc4a563356ea772b5
gabrielqueiroz08 <100386457+gabrielqueiroz08@users.noreply.github.com>
2022-05-15 10:31:13 -0300

    programadorPreguiçoso2.0

--- sketch.js
+++ sketch.js
@@ -12,9 +12,13 @@ function setup(){
  createCanvas(400,400);
 
 // Movendo o fundo 
+path = createSprite(200,200);
+path.addImage(pathImg)
+pathImg.velocityY = -3
 
-
-
+boy = createSprite(180,340,30,30);
+boy.scale = 0.08
+boy.addAnimation("jaxonRunning", boyImg)
 
 //Criando menino que corre 
 
@@ -26,7 +30,8 @@ leftBoundary.visible = false;
 
 
 //Crie Boundary direito 
-
+rightBoundary=createSprite(410,0,100,800)
+rightBoundary.visible= false
 }
 
 
@@ -41,7 +46,9 @@ boy.collide(edges[3]);
 
 
  //Reiniciar o fundo
- 
+ if (path.y > 400){
+     path.y = height/2
+ }
 
 
 
